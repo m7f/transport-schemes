7b981174795a76c64a114f7493e61a54fb0cbc6e
mflorinsky <flomih@yandex.ru>
2017-02-21 19:37:54 +0300

    Auto id parsing added

--- scraper/source/getIdList.js
+++ scraper/source/getIdList.js
@@ -1,25 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+const request = require('request');
 
 if (!fs.existsSync(path.join(__dirname, '../lists'))) {
     fs.mkdirSync(path.join(__dirname, '../lists'));
 }
 
 const getIdList = () => {
-    arr = [];
-    parts = [1, 2, 3, 4, 5];
-    parts.forEach(part => {
-        const file = fs.readFileSync(path.join(__dirname, `../id_data/idheap${part}.json`))
-        fs.writeFileSync(path.join(__dirname, '../id_data/id_part.json'), JSON.stringify(JSON.parse(file), null, 4));
-        const jFile = JSON.parse(fs.readFileSync(path.join(__dirname, '../id_data/id_part.json')));
-        jFile.aaData.forEach(elem => {
-            arr.push(elem[0])
-        });
-    })
-    fs.writeFileSync(path.join(__dirname, '../lists/id_list.json'), JSON.stringify(arr, null, 4));
-    if (fs.existsSync(path.join(__dirname, '../id_data/id_part.json'))) {
-        fs.unlinkSync(path.join(__dirname, '../id_data/id_part.json'));
-    }
+    request({
+        url: 'http://transport.orgp.spb.ru/Portal/transport/routes/list',
+        jar: true,
+    }, () => request({
+        url: 'http://transport.orgp.spb.ru/Portal/transport/routes/list',
+        body: 'sEcho=2&iColumns=11&sColumns=id%2CtransportType%2CrouteNumber%2Cname%2Curban%2CpoiStart%2CpoiFinish%2Ccost%2CforDisabled%2CscheduleLinkColumn%2CmapLinkColumn&iDisplayStart=0&iDisplayLength=1000&sNames=id%2CtransportType%2CrouteNumber%2Cname%2Curban%2CpoiStart%2CpoiFinish%2Ccost%2CforDisabled%2CscheduleLinkColumn%2CmapLinkColumn&iSortingCols=1&iSortCol_0=2&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=true&bSortable_8=true&bSortable_9=false&bSortable_10=false&transport-type=0&transport-type=46&transport-type=2&transport-type=1',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        }
+    }, (err, res, body) => {
+        data = JSON.parse(body).aaData.map(stop => stop[0]);
+        fs.writeFileSync(path.join(__dirname, '../lists/id_list.json'), JSON.stringify(data, null, 4));
+    }))
 };
 
 module.exports = getIdList();
