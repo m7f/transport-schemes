8b4abc68485446e1ec38e008a46573e41ffca885
mflorinsky <flomih@yandex.ru>
2017-03-15 05:25:03 +0300

    Path fix

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -1,28 +1,27 @@
-
 const map = L.map('map');
 map.setView([60, 30], 11);
 map.addLayer(
-  new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 );
 
-fetch('/data.json')
-  .then(r => r.json())
-  .then(data => {
+fetch('../data.json')
+    .then(r => r.json())
+    .then(data => {
 
-    Object.values(data.stops).forEach(s => {
-      L.circleMarker([s.lat, s.lon], {
-        stroke: false,
-        color: 'navy',
-        fillOpacity: 0.8,
-        radius: 5,
-      }).addTo(map);
+        Object.values(data.stops).forEach(s => {
+            L.circleMarker([s.lat, s.lon], {
+                stroke: false,
+                color: 'navy',
+                fillOpacity: 0.8,
+                radius: 5,
+            }).addTo(map);
+        });
+
+        Object.values(data.routes).forEach(r => {
+            Object.values(r.trips).forEach(t => {
+
+            L.polyline(t.shape)
+            .addTo(map);
+        });
     });
-
-    Object.values(data.routes).forEach(r => {
-      Object.values(r.trips).forEach(t => {
-
-        L.polyline(t.shape)
-          .addTo(map);
-      });
-    });
-  });
+});
