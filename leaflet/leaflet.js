a69f64e1274f83d8f22687ad3b742292c808c7be
mflorinsky <flomih@yandex.ru>
2017-04-09 05:58:48 +0300

    New design

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -1,6 +1,4 @@
-fetch('../data.json')
-.then(r => r.json())
-.then(data => {
+fetch('../data.json').then(r=>r.json()).then(data=>{
 
     const colorScheme = {
         bus: {
@@ -8,109 +6,168 @@ fetch('../data.json')
             weight: 6
         },
         tram: {
-            color: '#F90303',
+            color: '#FF0000',
             weight: 6
         },
         trolley: {
             color: '#67ED00',
             weight: 6
         },
-        dehighlightRoute : {
-            color: '#015ADD',
+        dehighlightRoute: {
+            color: '#1441C9',
             weight: 3,
         },
-        highlightStop : {
-            color: 'red',
-            radius: 6,
+        highlightStop: {
+            radius: 8,
+            fillColor: "#FF7800",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1
         },
-        dehighlightStop : {
-            stroke: false,
-            color: 'navy',
-            fillOpacity: 0.8,
-            radius: 4,
+        dehighlightStop: {
+            radius: 8,
+            fillColor: "#FF7800",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
         }
     }
 
-    var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    var dark = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        minZoom: 11,
+        maxZoom: 19
+    });
 
-    var grayscale = L.tileLayer(mbUrl, {
-        id: 'mapbox.light',
-        attribution: mbAttr}),
-        common   = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    var light = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        minZoom: 11,
+        maxZoom: 19
+    });
 
     var map = L.map('map', {
-        center: [60, 30],
-        zoom: 10,
-        layers: [grayscale]
+        center: [59.969881, 30.275338],
+        zoom: 11,
+        layers: [dark]
     });
 
-
-
-    onStopOver = (e) => {
-        e.target.setStyle(colorScheme.highlightStop)
+    var routes = {
+        bus: {
+            shape: [],
+            stops: []
+        },
+        tram: {
+            shape: [],
+            stops: []
+        },
+        trolley: {
+            shape: [],
+            stops: []
+        }
     }
-
-    onStopOut = (e) => {
-        e.target.setStyle(colorScheme.dehighlightStop)
-    }
-
-
-    var stops = L.layerGroup(Object.values(data.stops).filter(value => value.status === 'active').map(s =>
-        L.circleMarker([s.lat, s.lon], colorScheme.dehighlightStop)
-            .bindPopup(`${s.title}<br>${s.lat}, ${s.lon}`)
-            .on('mouseover', onStopOver)
-            .on('mouseout', onStopOut)
-
-    ));
-
-
-
-    var lines = [];
-    Object.values(data.routes).forEach(r => {
-        Object.values(r.trips).forEach(t => {
-            var clicked = 'false'
-            lines.push(L.polyline(t.shape, colorScheme.dehighlightRoute)
-            .bindPopup(`${r.id}`)
-            .on('mouseover', (e) => {
-                e.target.bringToFront();
+    Object.values(data.routes).forEach(r=>{
+        var featureShape = []
+          , featureStops = []
+          , shape = [];
+        Object.values(r.trips).forEach(t=>{
+            t.shape.forEach(elem=>shape.push(elem))
+            t.stops.forEach(s=>featureStops.push(L.circleMarker([data.stops[s].lat, data.stops[s].lon], colorScheme.dehighlightStop).bindPopup(`${data.stops[s].title}`).on('mouseover', (e)=>{
+                e.target.setStyle(colorScheme.highlightStop)
+            }
+            ).on('mouseout', (e)=>{
+                e.target.setStyle(colorScheme.dehighlightStop)
+            }
+            )))
+        }
+        )
+        var clicked = false
+        featureShape.push(L.polyline(shape, colorScheme.dehighlightRoute).bindPopup(`${r.id}`).on('mouseover', (e)=>{
+            e.target.bringToFront()
+            e.target.setStyle(colorScheme[r.type])
+            if (clicked) {
+                featureStops.bringToFront()
+            }
+        }
+        ).on('mouseout', (e)=>{
+            if (!clicked) {
+                e.target.bringToBack()
+                e.target.setStyle(colorScheme.dehighlightRoute)
+            }
+        }
+        ).on('click', (e)=>{
+            if (!clicked) {
+                e.target.bringToFront()
                 e.target.setStyle(colorScheme[r.type])
-            })
-            .on('mouseout', (e) => {
-                if (clicked === 'false') {
-                    e.target.bringToBack();
-                    e.target.setStyle(colorScheme.dehighlightRoute)
-                }
-            })
-            .on('click', (e) => {
-                if (clicked === 'false') {
-                    clicked = 'true'
-                    e.target.bringToFront();
-                    e.target.setStyle(colorScheme[r.type])
-                } else {
-                    e.target.bringToBack();
-                    e.target.setStyle(colorScheme.dehighlightRoute)
-                }
-            })
-        )});
-    });
+                clicked = true
+                map.addLayer(featureStops)
+            } else {
+                e.target.bringToBack()
+                e.target.setStyle(colorScheme.dehighlightRoute)
+                clicked = false
+                map.removeLayer(featureStops)
+            }
+        }
+        ))
+        routes[r.type].shape.push(featureShape)
+        routes[r.type].stops.push(featureStops)
+        featureStops = L.featureGroup(featureStops)
+        featureShape = L.featureGroup(featureShape)
+        map.on('overlayremove', ()=>{
+            map.removeLayer(featureStops)
+            clicked = false
+            featureShape.setStyle(colorScheme.dehighlightRoute);
+        }
+        );
+        map.on('overlayadd', ()=>{
+            if (clicked) {
+                featureShape.bringToFront();
+                featureStops.bringToFront();
+            }
+        }
+        )
 
-    var roads = L.featureGroup(lines);
+    }
+    )
+
+    var busRoads = []
+      , tramRoads = []
+      , trolleyRoads = [];
+    routes.bus.shape.forEach(f=>{
+        f.forEach(e=>busRoads.push(e))
+    }
+    )
+    routes.tram.shape.forEach(f=>{
+        f.forEach(e=>tramRoads.push(e))
+    }
+    )
+    routes.trolley.shape.forEach(f=>{
+        f.forEach(e=>trolleyRoads.push(e))
+    }
+    )
+    busRoads = L.featureGroup(busRoads)
+    tramRoads = L.featureGroup(tramRoads)
+    trolleyRoads = L.featureGroup(trolleyRoads)
 
     var baseMaps = {
-        "Grayscale": grayscale,
-        "Common": common
+        "Светлая тема": light,
+        "Темная тема": dark
     };
 
     var overlayMaps = {
-        "Stops": stops,
-
+        "Автобусы": busRoads,
+        "Трамваи": tramRoads,
+        "Троллейбусы": trolleyRoads
     };
 
     L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-    map.addLayer(roads)
+    map.addLayer(busRoads)
+    map.addLayer(tramRoads)
+    map.addLayer(trolleyRoads)
 
-});
+}
+);
